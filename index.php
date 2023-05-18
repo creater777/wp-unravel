741aa1b3bf6dbec146a091208dcf82d52004d63c
Aleksey <creater777@yandex.ru>
2023-05-18 12:11:47 +0300

    fix intro transition

--- index.php
+++ index.php
@@ -15,38 +15,6 @@
 get_header();
 $global_id = get_global();
 ?>
-    <div class="grailed">
-        <div class="intro">
-            <div class="video-wrapper">
-                <video autoplay loop muted playsinline poster="/wp-content/themes/wp_unravel/img/poster.jpg">
-                    <source src="/wp-content/themes/wp_unravel/img/intro.mp4" type="video/mp4">
-                </video>
-                <div class="ticker">
-                    <div class="ticker-wrapper">
-                        <div class="ticker-wrapper__item">
-                            <?= __(CFS()->get('logo_text', $global_id)); ?>
-                        </div>
-                        <ul class="ticker-wrapper__buttons">
-                            <?php
-                            $subcategories = get_categories(['child_of' => 0]);
-                            $subItems = [];
-                            foreach ($subcategories as $cat) {
-                                if (array_search($cat->term_id, [3, 48, 108]) !== false) {
-                                    continue;
-                                }
-                                $subItems[] = "<li class='main-menu-item'><a href='" . get_category_link($cat->term_id) . "'>{$cat->name}</a></li>";
-                            }
-                            if (!empty($subItems)) {
-                                echo implode("", $subItems);
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="h-screen"></div>
     <div id="body">
         <?php if (have_posts()) : ?>
             <div class="container posts">
@@ -72,43 +40,6 @@ $global_id = get_global();
             </section><!-- .error-404 -->
         <? endif; ?>
     </div>
-    <script>
-      $(function () {
-        let grailed = $('.grailed'), ticker = $('.ticker'),
-          tickerWrapper = $('.ticker-wrapper'),
-          tickerItem = $('.ticker-wrapper__item'),
-          tickerButtons = $('.ticker-wrapper__buttons'),
-          maxWidth = ticker.width(), left = 0, width = tickerItem.width() + tickerButtons.width() + 80;
-
-        function fixBodyPosition() {
-          if (window.scrollY > grailed.height()) {
-            $('#body').removeClass('fixed');
-            $('.h-screen').css({'display': 'none'});
-          } else {
-            $('#body').addClass('fixed');
-            $('.h-screen').css({'display': 'block'});
-          }
-        }
-
-        fixBodyPosition();
-        $(document).on('scroll', fixBodyPosition);
-
-        do {
-          tickerWrapper.append(tickerItem.clone());
-          tickerWrapper.append(tickerButtons.clone());
-          width += tickerItem.width() + tickerButtons.width() + 80;
-        } while (width < maxWidth * 2);
-
-        setInterval(function () {
-          left--;
-          tickerWrapper.css({'left': left});
-          if (Math.abs(left) > tickerItem.width() + tickerButtons.width() + 80) {
-            left += tickerItem.width() + tickerButtons.width() + 80;
-            tickerWrapper.css({'left': left});
-          }
-        }, 20);
-      })
-    </script>
 <?php
 
 get_footer();
