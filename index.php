2e8e6e9a9e355bbb2a354ca2aaa707a40074915c
Aleksey <creater777@yandex.ru>
2023-08-02 01:48:30 +0300

    fix image size

--- index.php
+++ index.php
@@ -15,111 +15,123 @@
 get_header();
 $global_id = get_global();
 ?>
-    <div class="grailed">
-        <div class="intro">
-            <div class="video-wrapper">
-                <video autoplay loop muted playsinline poster="/wp-content/themes/wp_unravel/img/poster.jpg">
-                    <source src="/wp-content/themes/wp_unravel/img/intro.mp4" type="video/mp4">
-                </video>
-                <div class="ticker">
-                    <div class="ticker-wrapper">
-                        <div class="ticker-wrapper__item">
-                            <?= __(CFS()->get('logo_text', $global_id)); ?>
-                        </div>
-                        <ul class="ticker-wrapper__buttons">
-                            <?php
-                            $subcategories = get_categories(['child_of' => 0]);
-                            $subItems = [];
-                            foreach ($subcategories as $cat) {
-                                if (array_search($cat->term_id, [3, 48, 108]) !== false) {
-                                    continue;
-                                }
-                                $subItems[] = "<li class='main-menu-item'><a href='" . get_category_link($cat->term_id) . "'>{$cat->name}</a></li>";
-                            }
-                            if (!empty($subItems)) {
-                                echo implode("", $subItems);
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                </div>
+  <div class="grailed">
+    <div class="intro">
+      <div class="video-wrapper">
+        <video autoplay loop muted playsinline poster="/wp-content/themes/wp_unravel/img/poster.jpg">
+          <source src="/wp-content/themes/wp_unravel/img/intro.mp4" type="video/mp4">
+        </video>
+        <div class="ticker">
+          <div class="ticker-wrapper">
+            <div class="ticker-wrapper__item">
+                <?= __(CFS()->get('logo_text', $global_id)); ?>
             </div>
+            <ul class="ticker-wrapper__buttons">
+                <?php
+                $subcategories = get_categories(['child_of' => 0]);
+                $subItems = [];
+                foreach ($subcategories as $cat) {
+                    if (array_search($cat->term_id, [3, 48, 108]) !== false) {
+                        continue;
+                    }
+                    $subItems[] = "<li class='main-menu-item'><a href='" . get_category_link($cat->term_id) . "'>{$cat->name}</a></li>";
+                }
+                if (!empty($subItems)) {
+                    echo implode("", $subItems);
+                }
+                ?>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
-    <div class="h-screen"></div>
+  </div>
+  <div class="h-screen"></div>
 
-    <div id="body">
-        <?php if (have_posts()) : ?>
-            <div class="container posts">
-                <div class="dcore dcore-list dcore-menu-10">
-                    <div class="dcore-thumbnails menu-list">
-                        <div class="items">
-                            <?
-                            while (have_posts()) {
-                                the_post();
-                                get_template_part('template-parts/content', '');
-                            };
-                            the_posts_navigation();
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div><!-- #main -->
-        <? else : ?>
-            <section class="error-404 not-found">
-                <div class="page-content">
-                    <p><?= __('Not found') ?></p>
-                </div><!-- .page-content -->
-            </section><!-- .error-404 -->
-        <? endif; ?>
-    </div>
+  <div id="body">
+      <?php if (have_posts()) : ?>
+        <div class="container posts">
+          <div class="dcore dcore-list dcore-menu-10">
+            <div class="dcore-thumbnails menu-list">
+              <div class="items">
+                  <?
+                  while (have_posts()) {
+                      the_post();
+                      get_template_part('template-parts/content', '');
+                  };
+                  the_posts_navigation();
+                  ?>
+              </div>
+            </div>
+          </div>
+        </div><!-- #main -->
+      <? else : ?>
+        <section class="error-404 not-found">
+          <div class="page-content">
+            <p><?= __('Not found') ?></p>
+          </div><!-- .page-content -->
+        </section><!-- .error-404 -->
+      <? endif; ?>
+  </div>
 
-    <script>
-      $(function () {
-        let grailed = $('.grailed'), ticker = $('.ticker'),
-          tickerWrapper = $('.ticker-wrapper'),
-          tickerItem = $('.ticker-wrapper__item'),
-          tickerButtons = $('.ticker-wrapper__buttons'),
-          left = 0, width = tickerItem.width() + tickerButtons.width() + 80;
+  <script>
+    $(function () {
+      let grailed = $('.grailed'), ticker = $('.ticker'),
+        tickerWrapper = $('.ticker-wrapper'),
+        tickerItem = $('.ticker-wrapper__item'),
+        tickerButtons = $('.ticker-wrapper__buttons'),
+        left = 0, width = tickerItem.width() + tickerButtons.width() + 80,
 
-        function fixBodyPosition() {
-          const body = $('#body'), iscreen = $('.h-screen');
-          const height = $('.nav-main').height();
-          if (height) {
-            body.css({'padding-top': height});
-          } else {
-            body.css({'padding-top': 0});
-          }
+        timer = 0;
 
-          if (window.scrollY >= grailed.height() && body.height() > grailed.height()) {
-            body.removeClass('fixed');
-            iscreen.css({'display': 'none'});
-          } else {
-            body.addClass('fixed');
-            iscreen.css({'display': 'block'});
-          }
+      function fixBodyPosition() {
+        const body = $('#body'), iscreen = $('.h-screen');
+        const height = $('.nav-main').height();
+        clearInterval(timer);
+        if (height) {
+          body.css({'padding-top': height});
+        } else {
+          body.css({'padding-top': 0});
         }
 
-        fixBodyPosition();
-        $(document).on('scroll', fixBodyPosition);
-        $(document).on('fetch', fixBodyPosition);
+        if (window.scrollY >= grailed.height() && body.height() > grailed.height()) {
+          clearInterval(timer);
+          timer = setInterval(function () {
+            window.scrollTo({
+              top: window.scrollY + 1,
+              behavior: 'smooth',
+            });
+          }, 10);
+          body.removeClass('fixed');
+          iscreen.css({'display': 'none'});
+        } else {
+          body.addClass('fixed');
+          iscreen.css({'display': 'block'});
+        }
+      }
 
-        do {
-          tickerWrapper.append(tickerItem.clone());
-          tickerWrapper.append(tickerButtons.clone());
-          width += tickerItem.width() + tickerButtons.width() + 80;
-        } while (width < ticker.width() * 2);
+      fixBodyPosition();
+      $(document).on('touchmove', fixBodyPosition);
+      $(document).on('touchstart', fixBodyPosition);
+      $(document).on('touchend', fixBodyPosition);
+      $(document).on('scroll', fixBodyPosition);
 
-        setInterval(function () {
-          left--;
+      do {
+        tickerWrapper.append(tickerItem.clone());
+        tickerWrapper.append(tickerButtons.clone());
+        width += tickerItem.width() + tickerButtons.width() + 80;
+      } while (width < ticker.width() * 2);
+
+      setInterval(function () {
+        left--;
+        tickerWrapper.css({'left': left});
+        if (Math.abs(left) > tickerItem.width() + tickerButtons.width() + 80) {
+          left += tickerItem.width() + tickerButtons.width() + 80;
           tickerWrapper.css({'left': left});
-          if (Math.abs(left) > tickerItem.width() + tickerButtons.width() + 80) {
-            left += tickerItem.width() + tickerButtons.width() + 80;
-            tickerWrapper.css({'left': left});
-          }
-        }, 20);
-      })
-    </script>
+        }
+      }, 20);
+    })
+  </script>
 <?php
 
 get_footer();
